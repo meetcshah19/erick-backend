0dc4e7428b03c729f7edbba7e8907ac28143f521
Sickaada <madhur_r@ph.iitr.ac.in>
2022-11-05 02:05:39 +0530

    add driver details and change schema for lat and lng

--- connectors/schema.js
+++ connectors/schema.js
@@ -2,11 +2,13 @@ var mongoose = require("mongoose");
 var Schema = mongoose.Schema;
 const  data = new Schema({
     erick_id: String,
-    latitude: Number,
-    longitude: Number,
+    lat: Number,
+    lng: Number,
     speed: Number,
     time: Number,
-    received_at: Date
+    received_at: Date,
+    driver_name: {type: String, default: "Alice"},
+    driver_contact: {type: Number, default: "0123456789"}
 }, {
     collection: 'data'
 });
--- routes/index.js
+++ routes/index.js
@@ -22,8 +22,8 @@ router.post("/save_erick_data/", function (req, res) {
 
   var save_data = new Model({
     erick_id: api_data["end_device_ids"]["device_id"],
-    latitude: api_data["uplink_message"]["decoded_payload"]["data"][0],
-    longitude: api_data["uplink_message"]["decoded_payload"]["data"][1],
+    lat: api_data["uplink_message"]["decoded_payload"]["data"][0],
+    lng: api_data["uplink_message"]["decoded_payload"]["data"][1],
     speed: api_data["uplink_message"]["decoded_payload"]["data"][4],
     received_at: new Date(),
   }).save(function (err, result) {
@@ -36,7 +36,18 @@ router.post("/save_erick_data/", function (req, res) {
   });
 });
 
+router.post("/save_driver_details/", async function (req, res){
+  let api_data = req.body
+  let id = req.body["id"]
+  let name = req.body["name  "]
+  const result = await Model.updateMany({ erick_id: 'eui-0102030405060708' }, {
+    driver_name: 'Jon Snow',
+    driver_contact: '7894561230'
+  });
+})
+
 router.get("/get_erick_data/", function (req, res) {
+ 
   if (req.query.erick_id == undefined && req.query.start_date == undefined && req.query.end_date == undefined) {
     Model.aggregate(
       [
@@ -46,9 +57,11 @@ router.get("/get_erick_data/", function (req, res) {
             _id: "$erick_id",
             data: {
               $first: {
-                lat: "$latitude",
-                lng: "$longitude",
+                lat: "$lat",
+                lng: "$lng",
                 speed: "$speed",
+                driver_name: "$driver_name",
+                driver_contact: "$driver_contact",
               },
             },
           },
@@ -77,9 +90,11 @@ router.get("/get_erick_data/", function (req, res) {
             _id: "$erick_id",
             data: {
               $first: {
-                lat: "$latitude",
-                lng: "$longitude",
+                lat: "$lat",
+                lng: "$lng",
                 speed: "$speed",
+                driver_name: "$driver_name",
+                driver_contact: "$driver_contact",
               },
             },
           },
