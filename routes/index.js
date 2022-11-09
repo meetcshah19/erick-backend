29c71d805b95b8867747b7640dabde818f96ca7f
Sickaada <madhur_r@ph.iitr.ac.in>
2022-11-09 22:10:01 +0530

    created_at field

--- routes/index.js
+++ routes/index.js
@@ -39,7 +39,7 @@ router.post("/save_erick_data/", function (req, res) {
 router.post("/save_driver_details/", async function (req, res){
   let api_data = req.body
   let id = req.body["id"]
-  let name = req.body["name  "]
+  let name = req.body["name"]
   const result = await Model.updateMany({ erick_id: 'eui-0102030405060708' }, {
     driver_name: 'Jon Snow',
     driver_contact: '7894561230'
@@ -63,6 +63,7 @@ router.get("/get_erick_data/", function (req, res) {
                 speed: "$speed",
                 driver_name: "$driver_name",
                 driver_contact: "$driver_contact",
+                received_at: "$received_at"
               },
             },
           },
@@ -96,6 +97,7 @@ router.get("/get_erick_data/", function (req, res) {
                 speed: "$speed",
                 driver_name: "$driver_name",
                 driver_contact: "$driver_contact",
+                received_at: "$received_at"
               },
             },
           },
